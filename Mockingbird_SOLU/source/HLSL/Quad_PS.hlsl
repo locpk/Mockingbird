456a3f8bd176df3cfa31ed2934bd47b5c5ec2933
Junshu Chen <locpk123@gmail.com>
2015-09-21 19:32:57 -0400

    Use luminosity method for gray scale

--- Mockingbird_SOLU/source/HLSL/Quad_PS.hlsl
+++ Mockingbird_SOLU/source/HLSL/Quad_PS.hlsl
@@ -14,6 +14,8 @@ struct P_IN
 float4 main(P_IN input) : SV_TARGET
 {
 	float4 tex = baseTexture.Sample(filters,input.tex);
-	tex.b = tex.g = tex.r = (tex.b + tex.g + tex.r) / 3;
+	// 0.21 R + 0.72 G + 0.07 B.
+	tex.b = tex.g = tex.r = 0.21 * tex.r + 0.72 * tex.g + 0.07 * tex.b;
+	//tex.b = tex.g = tex.r = (tex.b + tex.g + tex.r) / 3;
 	return tex;
 }
